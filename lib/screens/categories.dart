cb45cf12c051335a54279e3cb2ce933510f71293
Patrick Hackl <hackl.patrick97@gmail.com>
2026-07-19 15:13:27 +0200

    added inital project

---
+++ lib/screens/categories.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:meal_tracker/dummy/dummy_data.dart';
+import 'package:meal_tracker/widgets/category/category_grid_item.dart';
+
+import '../models/category/category.dart';
+import 'meals.dart';
+
+/// Here the category selection will be handled
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    var meals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: meals
+        ))
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pick your category')),
+      body: GridView(
+        padding: EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for(var cat in availableCategories)
+            CategoryGridItem(
+                category: cat,
+                onSelectCategory: ()  => _selectCategory(context, cat),
+            )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/category/category_grid_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:meal_tracker/models/category/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+    required this.onSelectCategory
+  });
+
+  final Category category;
+  final void Function() onSelectCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onSelectCategory,
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  category.color.withValues(alpha: .55),
+                  category.color.withValues(alpha: .9)
+                ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+            ),
+          borderRadius: BorderRadius.circular(16)
+        ),
+        child: Text(category.title, style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onSurface
+        )),
+      ),
+    );
+  }
+}
